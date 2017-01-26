a53d85d1dd7ab2a4a4fd5fea7caf216bc50f83ca
xiaoqianghou <2662651103@qq.com>
2017-01-27 00:06:11 +0800

    Initial commit

---
+++ app/src/main/java/com/wilddog/newchat/ChatApplication.java
@@ -0,0 +1,23 @@
+package com.wilddog.newchat;
+
+
+import com.wilddog.wilddogcore.WilddogApp;
+import com.wilddog.wilddogcore.WilddogOptions;
+
+/**
+ * @author Jeen
+ * @since 9/16/15
+ *
+ * Initialize Wilddog with the application context. This must happen before the client is used.
+ */
+public class ChatApplication extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: change this to your own Wilddog URL
+
+//       WilddogOptions wilddogOptions=new WilddogOptions.Builder().setSyncUrl("https://testmydemo.wilddogio.com").build();
+       WilddogOptions wilddogOptions=new WilddogOptions.Builder().setSyncUrl("https://myfirstandroidapp2017.wilddogio.com").build();
+        WilddogApp wilddogApp=WilddogApp.initializeApp(this,wilddogOptions);
+    }
+}
---
+++ app/src/main/java/com/wilddog/newchat/MainActivity.java
@@ -0,0 +1,132 @@
+package com.wilddog.newchat;
+
+import android.app.ListActivity;
+import android.content.SharedPreferences;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wilddog.client.DataSnapshot;
+import com.wilddog.client.SyncError;
+import com.wilddog.client.SyncReference;
+import com.wilddog.client.ValueEventListener;
+import com.wilddog.client.WilddogSync;
+
+import java.util.Random;
+
+public class MainActivity extends ListActivity {
+
+
+
+
+    private String mUsername;
+    private SyncReference mWilddogRef;
+    private ValueEventListener mConnectedListener;
+    private com.wilddog.newchat.ChatListAdapter mChatListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Make sure we have a mUsername
+        setupUsername();
+
+        setTitle("Chatting as " + mUsername);
+
+        // Setup our Wilddog mWilddogRef
+        mWilddogRef = WilddogSync.getInstance().getReference().child("chat");
+
+
+        // Setup our input methods. Enter key on the keyboard or pushing the send button
+        EditText inputText = (EditText) findViewById(R.id.messageInput);
+        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_NULL && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    sendMessage();
+                }
+                return true;
+            }
+        });
+
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        final ListView listView = getListView();
+        // Tell our list adapter that we only want 50 messages at a time
+        mChatListAdapter = new com.wilddog.newchat.ChatListAdapter(mWilddogRef.limitToLast(50), this, R.layout.chat_message, mUsername);
+        listView.setAdapter(mChatListAdapter);
+        mChatListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(mChatListAdapter.getCount() - 1);
+            }
+        });
+
+        // Finally, a little indication of connection status
+        mConnectedListener = mWilddogRef.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = (Boolean) dataSnapshot.getValue();
+                if (connected) {
+                    Toast.makeText(MainActivity.this, "Connected to Wilddog", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Disconnected from Wilddog", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(SyncError wilddogError) {
+                // No-op
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mWilddogRef.getRoot().child(".info/connected").removeEventListener(mConnectedListener);
+        mChatListAdapter.cleanup();
+    }
+
+    private void setupUsername() {
+        SharedPreferences prefs = getApplication().getSharedPreferences("ChatPrefs", 0);
+        mUsername = prefs.getString("username", null);
+        if (mUsername == null) {
+            Random r = new Random();
+            // Assign a random user name if we don't have one saved.
+            mUsername = "WilddogUser" + r.nextInt(100000);
+            prefs.edit().putString("username", mUsername).commit();
+        }
+    }
+
+    private void sendMessage() {
+        EditText inputText = (EditText) findViewById(R.id.messageInput);
+        String input = inputText.getText().toString();
+        if (!input.equals("")) {
+            // Create our 'model', a Chat object
+            Chat chat = new Chat(input, mUsername);
+            // Create a new, auto-generated child of that chat location, and save our chat data there
+            mWilddogRef.push().setValue(chat);
+            inputText.setText("");
+        }
+    }
+}
